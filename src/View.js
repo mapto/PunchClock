9f78589ec1cde5b081bd61a0b4d201cc666170fb
Martin Ruskov <martin@unriddle.it>
2017-10-30 08:58:54 +0100

    pretty-print dates in view

--- src/View.js
+++ src/View.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
+var getDuration = function(millisec) {
+  // in hours, up to 15 mins accuracy
+  return Math.round(Math.abs(millisec / 9e5))/4;
+}
+
 class SlotView extends Component {
   render() {
+  	var startTimestamp = new Date(this.props.data.Start);
+  	var endTimestamp = new Date(this.props.data.End);
+   	var duration = getDuration(endTimestamp - startTimestamp);
     return (
  <div>
     <h3 className="list-group-item-heading">{this.props.data.ID}. {this.props.data.Description}</h3>
-    <p className="list-group-item-text">from {this.props.data.Start} to {this.props.data.End}</p>
+    <p className="list-group-item-text">{duration} hours on {startTimestamp.toDateString()} ({startTimestamp.toLocaleTimeString()} to {endTimestamp.toLocaleTimeString()})</p>
     <p className="list-group-item-text">Project: {this.props.data.Project}</p>
 </div>
     );      
